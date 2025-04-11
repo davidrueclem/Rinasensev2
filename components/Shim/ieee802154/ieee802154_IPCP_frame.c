2bcaeb8fb0b8033c09642ded1b132558ae4bf22e
davidclemlord <davidclemlord@gmail.com>
2025-04-11 10:46:19 +0200

    Minimize IEEE 802.15.4 header footprint

--- components/Shim/ieee802154/ieee802154_IPCP_frame.c
+++ components/Shim/ieee802154/ieee802154_IPCP_frame.c
@@ -293,98 +293,50 @@ static void debug_print_packet(uint8_t *packet, uint8_t packet_length)
 
 static void reverse_memcpy_local(uint8_t *restrict dst, const uint8_t *restrict src, size_t n);
 
-uint8_t ieee802154_header(const uint16_t *src_pan, ieee802154_address_t *src, const uint16_t *dst_pan,
-                          ieee802154_address_t *dst, uint8_t ack, uint8_t *header, uint8_t header_length) {
-    uint8_t frame_header_len = 2;
-    mac_fcs_t frame_header = {
-            .frameType = FRAME_TYPE_DATA,
-            .secure = false,
-            .framePending = false,
-            .ackReqd = ack,
-            .panIdCompressed = false,
-            .rfu1 = false,
-            .sequenceNumberSuppression = false,
-            .informationElementsPresent = false,
-            .destAddrType = dst->mode,
-            .frameVer = FRAME_VERSION_STD_2003,
-            .srcAddrType = src->mode
-    };
-
-    bool src_present = src != NULL && src->mode != ADDR_MODE_NONE;
-    bool dst_present = dst != NULL && dst->mode != ADDR_MODE_NONE;
-    bool src_pan_present = src_pan != NULL;
-    bool dst_pan_present = dst_pan != NULL;
-
-    if (src_pan_present && dst_pan_present && src_present && dst_present) {
-        if (*src_pan == *dst_pan) {
-            frame_header.panIdCompressed = true;
-        }
-    }
-
-    if (!frame_header.sequenceNumberSuppression) {
-        frame_header_len += 1;
-    }
-
-    if (dst_pan_present) {
-        frame_header_len += 2;
-    }
-
-    if (frame_header.destAddrType == ADDR_MODE_SHORT) {
-        frame_header_len += 2;
-    } else if (frame_header.destAddrType == ADDR_MODE_LONG) {
-        frame_header_len += 8;
-    }
-
-    if (src_pan_present && !frame_header.panIdCompressed) {
-        frame_header_len +=2;
-    }
-
-    if (frame_header.srcAddrType == ADDR_MODE_SHORT) {
-        frame_header_len += 2;
-    } else if (frame_header.srcAddrType == ADDR_MODE_LONG) {
-        frame_header_len += 8;
-    }
-
-    if (header_length < frame_header_len) {
-        return 0;
-    }
-
-    uint8_t position = 0;
-    memcpy(&header[position], &frame_header, sizeof frame_header);
-    position += 2;
-
-    if (!frame_header.sequenceNumberSuppression) {
-        header[position++] = 0;
-    }
-
-    if (dst_pan != NULL) {
-        memcpy(&header[position], dst_pan, sizeof(uint16_t));
-        position += 2;
-    }
-
-    if (frame_header.destAddrType == ADDR_MODE_SHORT) {
-        memcpy(&header[position], &dst->short_address, sizeof dst->short_address);
-        position += 2;
-    } else if (frame_header.destAddrType == ADDR_MODE_LONG) {
-        reverse_memcpy_local(&header[position], (uint8_t *)&dst->long_address, sizeof dst->long_address);
-        position += 8;
-    }
-
-    if (src_pan != NULL && !frame_header.panIdCompressed) {
-        memcpy(&header[position], src_pan, sizeof(uint16_t));
-        position += 2;
-    }
-
-    if (frame_header.srcAddrType == ADDR_MODE_SHORT) {
-        memcpy(&header[position], &src->short_address, sizeof src->short_address);
-        position += 2;
-    } else if (frame_header.srcAddrType == ADDR_MODE_LONG) {
-        reverse_memcpy_local(&header[position], (uint8_t *)&src->long_address, sizeof src->long_address);
-        position += 8;
-    }
-
-    return position;
+uint8_t ieee802154_header(const uint16_t *pan_id, ieee802154_address_t *src, ieee802154_address_t *dst,
+    uint8_t ack, uint8_t *header, uint8_t header_length) {
+if (!pan_id || !src || !dst || !header || header_length < 9) {
+return 0;
 }
+
+mac_fcs_t frame_header = {
+.frameType = FRAME_TYPE_DATA,
+.secure = false,
+.framePending = false,
+.ackReqd = ack,
+.panIdCompressed = true,  
+.rfu1 = false,
+.sequenceNumberSuppression = false,
+.informationElementsPresent = false,
+.destAddrType = ADDR_MODE_SHORT,
+.frameVer = FRAME_VERSION_STD_2003,
+.srcAddrType = ADDR_MODE_SHORT
+};
+
+uint8_t position = 0;
+
+// FCS
+memcpy(&header[position], &frame_header, sizeof(frame_header));
+position += 2;
+
+// Sequence Number
+header[position++] = 0x00;
+
+// PAN ID (compartido)
+memcpy(&header[position], pan_id, sizeof(uint16_t));
+position += 2;
+
+// Destino (short)
+memcpy(&header[position], &dst->short_address, sizeof(uint16_t));
+position += 2;
+
+// Origen (short)
+memcpy(&header[position], &src->short_address, sizeof(uint16_t));
+position += 2;
+
+return position; 
+}
+
 
 static void reverse_memcpy_local(uint8_t *restrict dst, const uint8_t *restrict src, size_t n)
 {
